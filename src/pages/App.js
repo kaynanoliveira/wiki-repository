895bccbda9d1267b34a93e1a127032d45b847f99
kayna <kaynanoliveira357@gmail.com>
2025-04-29 20:31:13 -0300

    alterações

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import GitLogo from '../assets/image.png'
+import {Container} from './styles'
+
+function App() {
+  return (
+    <Container className="App">
+      <img src={GitLogo} width={72} height={72} alt='logo github' />
+    </Container>
+  );
+}
+
+export default App;
