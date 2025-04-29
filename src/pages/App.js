dcb2cf40052d680db0cafc88d8db98ab65e3d035
kayna <kaynanoliveira357@gmail.com>
2025-04-29 20:56:57 -0300

    criando o Input

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,12 @@
 import GitLogo from '../assets/image.png'
+import Input from '../components/Input';
 import {Container} from './styles'
 
 function App() {
   return (
     <Container className="App">
       <img src={GitLogo} width={72} height={72} alt='logo github' />
+      <Input />
     </Container>
   );
 }
